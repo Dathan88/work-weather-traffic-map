a6509b0259696a036aea021f176cc9a9957f13f7
Dathan Spencer <dpspencer88@gmail.com>
2019-03-14 18:25:29 -0600

    Updates

---
+++ precache-manifest.414b9043aadd0ceea3384eed25ac5d38.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "84e2be070ceace7e8c79",
+    "url": "/static/js/main.697739ea.chunk.js"
+  },
+  {
+    "revision": "ae0b72b89335021e0997",
+    "url": "/static/js/2.4952e05c.chunk.js"
+  },
+  {
+    "revision": "84e2be070ceace7e8c79",
+    "url": "/static/css/main.2ed45ae2.chunk.css"
+  },
+  {
+    "revision": "a38ebb4a82bf7c8332e7226a6b538ccc",
+    "url": "/index.html"
+  }
+];
