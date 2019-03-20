a269bdf19cfa97e02ac09d45b0d15285cf496ebe
Dathan Spencer <dpspencer88@gmail.com>
2019-03-20 17:28:48 -0600

    Add import

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Tweets from './Components/Tweets';
 import MapApis from './Components/CustomMap';
 import '../src/App.css';
 
@@ -7,6 +8,7 @@ class App extends Component {
 		return (
 			<div className="App">
 				<MapApis/>
+				<Tweets/>
 			</div>
 		);
 	}
