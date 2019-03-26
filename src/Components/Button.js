2e9e32406ac0a1de1e96dd1d17bd84aabac15df3
Dathan Spencer <dpspencer88@gmail.com>
2019-03-26 10:23:53 -0600

    Remove unused imports

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 export const Button = ({ text, onClick, type, disabled, id }) => (
 	<button id ={id} type={type} disabled={disabled} onClick={onClick}>
--- src/Components/Tweets.js
+++ src/Components/Tweets.js
@@ -1,46 +1,21 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react';
 import { Timeline } from 'react-twitter-widgets';
 
-export const Tweets = ({className, id}) => (
-			<div id={id} className={className}>
-				<Timeline
-					dataSource={{
-						sourceType: 'list',
-						ownerScreenName: 'jakkTwoKays',
-						slug: 'work-traffic-weather',
-					}}
-					options={{
-						chrome: 'noheader , nofooter , noborders , noscrollbar ',
-						linkColor: '#E81C4F',
-						ariaPolite: 'assertive',
-						theme: 'dark',
-					}}
-					onLoad={() => console.log('Timeline is loaded!')}
-				/>
-			</div>
-)
-
-/* 
-const Twit = require('twit');
-const T = new Twit({
-	consumer_key: 'REACT_APP_TWITTER_API ',
-	consumer_secret: 'REACT_APP_TWITTER_SECRET_API ',
-	access_token: 'REACT_APP_TWITTER_ACCESS_TOKEN',
-	access_token_secret: 'REACT_APP_TWITTER_SECRET_ACCESS_TOKEN ',
-});
-
-const workArea = ['-112.12', '40.22', '-111.58', '40.78'];
-const stream = T.stream('statuses/filter', {
-	locations: workArea,
-	track: ['FR', 'UTA', 'I-15', 'TRAX'],
-	lang: 'en',
-});
-
-stream.on('tweet', tweet => {
-	console.log(tweet);
-}); 
-
-
-
-*/
+export const Tweets = ({ className, id }) => (
+	<div id={id} className={className}>
+		<Timeline
+			dataSource={{
+				sourceType: 'list',
+				ownerScreenName: 'jakkTwoKays',
+				slug: 'work-traffic-weather',
+			}}
+			options={{
+				chrome: 'noheader , nofooter , noborders , noscrollbar ',
+				linkColor: '#E81C4F',
+				ariaPolite: 'assertive',
+				theme: 'dark',
+			}}
+			onLoad={() => console.log('Timeline is loaded!')}
+		/>
+	</div>
+);
